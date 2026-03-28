8134e49ba36b0ca157744e51e6bf0f967246cff6
Udbhav-Dubey <udbhavdubey44@gmail.com>
2026-03-28 18:39:02 +0530

    header file for areana

--- areana/areana.h
+++ areana/areana.h
@@ -1,1 +1,27 @@
 #pragma once
+class Chunk{
+    private:
+     char* end{nullptr};
+    char* start{nullptr};
+    char* curr{nullptr};
+    char* arr;
+    public:
+    Chunk(int N);
+    char* alloc(int N,int allign);
+    Chunk(const Chunk&)=delete;
+    Chunk&operator(const Chunk&)=delete;
+    Chunk(Chunk&&x)noexcept;
+    Chunk&operator=(Chunk&&x)noexcept;
+    void reset();
+    ~Chunk();
+};
+class Areana{
+    private:
+        std::vector<&Chunk>areana;
+        int extra{};
+        int ali{4};
+    public:
+        Areana(int n,int a);
+        char *allocate(int byt);
+        ~Areana();
+};
