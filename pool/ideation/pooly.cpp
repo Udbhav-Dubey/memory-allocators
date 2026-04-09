a620dec782b4bb8e7cf079ad355ad9611fcb5b7d
Udbhav-Dubey <udbhavdubey44@gmail.com>
2026-04-09 16:02:41 +0530

    fuck up happened

--- pool/ideation/pooly.cpp
+++ pool/ideation/pooly.cpp
@@ -13,6 +13,7 @@ class Pool{
         size_t fss;// fixed slot size;
        char* head{nullptr};
        char*base{nullptr};
+       char* max_size{};
     public:
         Pool(size_t ts,size_t s):total_space{ts},slots{s}{
         //    if (s>ts){      std::cout << "hey not allowed \n will use assert here rn not sure of syntax of assert\n";    
@@ -29,6 +30,7 @@ class Pool{
             assert(slots>0);
             // malloc here bitch , get the base and final 
            base=(char*)malloc(sizeof(char)*ts);
+           max_size=base+total_space;
            assert(base!=nullptr);
         head=base;
            char* curr{nullptr};
@@ -56,6 +58,7 @@ class Pool{
             return temp;
         }
         void pfree(char*ptr_x){
+            if (ptr_x<base||ptr_x>max_size){std::cout << "not my pool mate, get out\n";}
             *(char**)ptr_x=head;
             head=ptr_x;
         }
@@ -67,10 +70,17 @@ int main (){
     // for now lets consider user gives space and slots also 
     // becuase if they onyl give total space then whats the best way to make slots i dont know alright so for now lets let them select slot 
     size_t ts,s,x;
+    std::cout << "Total size \n then slots \n";
     std::  cin>>ts>>s;
     Pool pool(ts,s);
+    std::cout << "enter allocation size:\n";
     std:: cin>>x;
     char*ptr_x=pool.allocate(x);
+    char c='a';
+   char *px=&c;
+    pool.pfree(px);
+    // lets check double free now 
+    pool.pfree(ptr_x);
     pool.pfree(ptr_x);
     return 0;
 }
