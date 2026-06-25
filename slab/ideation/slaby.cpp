1849e38b229c46723c0cd2dc18906becd54259af
Udbhav-Dubey <udbhavdubey44@gmail.com>
2026-06-25 19:11:18 +0530

    ideation done now .h and .cpp remaining

--- slab/ideation/slaby.cpp
+++ slab/ideation/slaby.cpp
@@ -5,6 +5,7 @@
 #include <cassert>
 #include <cstdlib>
 #include <vector>
+#include <unordered_map>
 #include <utility>
 class slab{
     int size_type{};
@@ -15,6 +16,8 @@ class slab{
     char*max_size;
     int used_count{};
     public:
+    slab(slab&)=delete;
+    slab operator=(slab&)=delete;
         slab(int t,int ts):size_type{t},total_size{ts} {
         slots=total_size/size_type;
         assert(ts>=t&&"total space should be greater than type\n");
@@ -79,6 +82,9 @@ class slab{
             }
             return 0;
         }
+        ~slab(){
+            free(base);
+        }
 };
 //partial empty used fixes sizes or on demand work ?
 // vector for these guys idk need to control them partial has half complete slabs , used has completed slabs, empty has  do what -> intially put slabs in empty only then start taking from it only;empty size? 
@@ -98,6 +104,8 @@ class cache{
         }
    }
     public:
+   cache(cache&)=delete;
+   cache operator=(cache&)=delete;
    void debug(){
        std::cout << "used\n"; 
        debug_type(used);
@@ -162,7 +170,7 @@ class cache{
    // does a block goes back to same slab , i think so but if all the slabs are of same size and does same thing for one cahce does changing good or not ??? 
    // lets assume yes same slab so for that what i could do is take a block and check range the range it is in we will simply insert but this way would be o(n) because i would need to search in partial and used the ranges and call free of slab
    // so create checkrange in slab also to check ;
-   void free(char*x){
+   void cfree(char*x){
        bool found=false;
 //            if (partial.size()==0){std::cerr<<"cannot free since no block allocated\n";}       
             for (auto &s:partial){
@@ -200,14 +208,74 @@ class cache{
        assert(found==true&&"not here mate\n");
    }
    ~cache(){
-   }
+    for(auto s : empty) delete s;
+    for(auto s : partial) delete s;
+    for(auto s : used) delete s;
+}
 };
 class slabAllocator{
-    
+    private:
+        int totalsize{};
+       // vector<cache*>cach;
+        std::unordered_map<int,cache*>mp;
+        std::unordered_map<char*,cache*>ptc;
+        int findsize(int n){
+            if (n<0){return -1;}
+            if (n==0){return 1;}
+            n--;
+            n|=n>>1;
+            n|=n>>2;
+            n|=n>>4;
+            n|=n>>8;
+            n|=n>>16;
+            //n|=n>>32;
+            return n+1;
+        }
+    public:
+        // should someone decide total size per cache or off the whole thing if the whole thing how do we divide for each slab , there is no way for slab to resize their total size , either we reserve or idk tbh 
+        slabAllocator(int ts){
+            totalsize=ts;
+        }
+        char*allocate(int x){
+            assert(x<totalsize&&"c'mon dawg you selected the size in first place\n");
+            int n=findsize(x);
+            if (mp.find(n)==mp.end()){
+               cache*temp=new cache(n,totalsize);
+               mp[n]=temp; 
+            }
+            char *ptr_x=mp[n]->allocate();
+            assert(ptr_x!=nullptr&&"something went wrong in allocation after accessing from map\n");
+            ptc[ptr_x]=mp[n];
+            return ptr_x;
+        }
+        void free(char*x){
+            assert(ptc.find(x)!=ptc.end()&&"double free on same\n");
+           // detemine size with help of ptr no direct way in my opinion need to loop , if there is a direct way i would need to store reverse hashmap also else by just address its not possible to find size; 
+           ptc[x]->cfree(x);
+           ptc.erase(x);
+        }
+        ~slabAllocator(){
+            for (auto &[k,v]:mp){
+                delete v;
+            }
+                mp.clear();
+                ptc.clear();
+
+        }
+        void debug(){
+            for (auto &[s,c]:mp){
+                std::cout << "cache  : " << s << "\n";
+                c->debug();
+            }
+        }
 };
 int main (){
+   /* int x=9;
+    x=~(x&0);
+    std::cout << x+1 << "\n";
+    */
     // currently rely on allignment since slab will not deal with any of that allignment would be dealt by slabAllocator class ;
- /*   slab s(64,128);
+    slab s(64,128);
     s.print_debug();
     char*x=s.allocate(55); 
     s.print_debug();
@@ -217,77 +285,108 @@ int main (){
     s.pfree(x);
     s.print_debug();
     s.pfree(y); 
-    */
+   
     //this was just slab part no need to focus on cache part
-   /*cache ch(64,192); 
+   cache ch(64,192); 
    ch.debug();
-    char*v=ch.allocate();  
+    char*vv=ch.allocate();  
     ch.debug();
-    char*a=ch.allocate();  
+    char*aa=ch.allocate();  
     ch.debug();
-    char*x=ch.allocate();  
+    char*xx=ch.allocate();  
     ch.debug();
-     char*z=ch.allocate();  
+     char*zz=ch.allocate();  
     ch.debug();
-    ch.free(v);
+    ch.cfree(vv);
     ch.debug();
-    */ 
-/*    cache ch(64,192);
+     
+    cache ch1(64,192);
 
-    std::cout << "\n--- initial ---\n";
-    ch.debug();
+    std::cout << "initial\n";
+    ch1.debug();
 
-    char* a = ch.allocate();
-    char* b = ch.allocate();
-    char* c = ch.allocate();
+    char* a1 = ch1.allocate();
+    char* b1 = ch1.allocate();
+    char* c1 = ch1.allocate();
 
-    std::cout << "\n--- after 3 allocs ---\n";
-    ch.debug();
+    std::cout << "after 3 allocs\n";
+    ch1.debug();
 
-    char* d = ch.allocate();
+    char* d1 = ch1.allocate();
 
-    std::cout << "\n--- after 4th alloc ---\n";
-    ch.debug();
+    std::cout << "after 4th alloc\n";
+    ch1.debug();
 
-    ch.free(a);
+    ch1.cfree(a1);
 
-    std::cout << "\n--- free(a) ---\n";
-    ch.debug();
+    std::cout << "free(a)\n";
+    ch1.debug();
 
-    ch.free(b);
+    ch1.cfree(b1);
 
-    std::cout << "\n--- free(b) ---\n";
-    ch.debug();
+    std::cout << "free(b) \n";
+    ch1.debug();
 
-    ch.free(c);
+    ch1.cfree(c1);
 
-    std::cout << "\n--- free(c) ---\n";
-    ch.debug();
+    std::cout << "free(c)\n";
+    ch1.debug();
 
-    char* e = ch.allocate();
+    char* ee = ch1.allocate();
 
-    std::cout << "\n--- alloc(e) ---\n";
-    ch.debug();
+    std::cout << "alloc(e) \n";
+    ch1.debug();
     char* ptrs[20];
 
 for(int i=0;i<20;i++)
-    ptrs[i]=ch.allocate();
+    ptrs[i]=ch1.allocate();
 
 for(int i=0;i<20;i++)
-    ch.free(ptrs[i]);
+    ch1.cfree(ptrs[i]);
 
-ch.debug();
-*/
-    cache ch(64,192);
+ch1.debug();
 
-char* ptrs[20];
+    cache ch2(64,192);
 
-for(int i=0;i<20;i++)
-    ptrs[i]=ch.allocate();
+char* ptrs1[20];
 
 for(int i=0;i<20;i++)
-    ch.free(ptrs[i]);
+    ptrs1[i]=ch2.allocate();
 
-ch.debug();
+for(int i=0;i<20;i++)
+    ch2.cfree(ptrs1[i]);
+
+ch2.debug();
+    slabAllocator alloc(192);
+    alloc.debug();
+    char* aaa = alloc.allocate(50);
+    char* bbb = alloc.allocate(60);
+    char* ccc = alloc.allocate(63);
+    alloc.debug();
+    char* ddd = alloc.allocate(64);
+    alloc.debug();
+    char* eee = alloc.allocate(70);
+    alloc.debug();
+    char* fff = alloc.allocate(20);
+    alloc.debug();
+    alloc.free(aaa);
+    alloc.free(eee);
+    alloc.free(fff);
+    alloc.debug();
+    char* ggg = alloc.allocate(18);
+    char* hhh = alloc.allocate(100);
+    alloc.debug();
+    alloc.free(bbb);
+    alloc.free(ccc);
+    alloc.free(ddd);
+    alloc.free(ggg);
+    alloc.free(hhh);
+    alloc.debug();
+    //s.pfree(z);
+    //ch.cfree(aa);
+//ch.cfree(xx);
+//ch.cfree(zz);
+//ch1.cfree(d1);
+//ch1.cfree(ee);
 return 0;
 }
