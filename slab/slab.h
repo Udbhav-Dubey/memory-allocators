6b3e034ab676e5a59aa76035d83f95abf87c0007
Udbhav-Dubey <udbhavdubey44@gmail.com>
2026-06-26 18:15:48 +0530

    slab.h

---
+++ slab/slab.h
@@ -0,0 +1,56 @@
+#pragma once
+#include <iostream>
+#include <cassert>
+#include <cstdlib>
+#include <vector>
+#include <unordered_map>
+#include <utility>
+class slab{
+    int size_type{};
+    int total_size{};
+    int slots{};
+    char*base{nullptr};
+    char*head{nullptr};
+    char*max_size;
+    int used_count{};
+    public:
+    slab(const slab&)=delete;
+    slab operator=(const slab&)=delete;
+        slab(int t,int ts);
+        char*allocate(int x);
+        void pfree(char*ptr_x);
+        bool full();
+        bool partial();
+        bool empty();
+        void print_debug();
+        bool checkrange(char*x);
+        ~slab();
+};
+class cache{
+    std::vector<slab*>used;
+    std::vector<slab*>empty;
+    std::vector<slab*>partial;
+   int tsize{};
+   int total_size{};
+   void debug_type(std::vector<slab*>x);
+    public:
+   cache(const cache&)=delete;
+   cache operator=(const cache&)=delete;
+   void debug();
+   cache(int ts,int t);
+   char*allocate();
+   void cfree(char*x);
+   ~cache();
+};
+class slabAllocator{
+    private:
+        int totalsize{};
+        std::unordered_map<int,cache*>mp;
+        std::unordered_map<char*,cache*>ptc;
+        int findsize(int n);
+    public:
+        slabAllocator(int ts);
+        char*allocate(int x);
+        void free(char*x);
+        ~slabAllocator();
+        void debug();
